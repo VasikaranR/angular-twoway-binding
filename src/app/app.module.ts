794bd65fa6c7e3a817319938c7ab674030734386
vasikaran <vasichan2000@gmail.com>
2022-04-21 09:40:33 +0530

    two way binding

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -1,14 +1,19 @@
 import { NgModule } from '@angular/core';
 import { BrowserModule } from '@angular/platform-browser';
-
+import { FormsModule } from '@angular/forms';
 import { AppComponent } from './app.component';
+import { NameComponent } from './name/name.component';
+import { AgeComponent } from './age/age.component';
 
 @NgModule({
   declarations: [
-    AppComponent
+    AppComponent,
+    NameComponent,
+    AgeComponent
   ],
   imports: [
-    BrowserModule
+    BrowserModule,
+    FormsModule
   ],
   providers: [],
   bootstrap: [AppComponent]
